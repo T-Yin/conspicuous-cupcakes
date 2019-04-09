4b7b0f59b403501ab8b6582726aa78df9eed9dae
Tiffany ✪ Starrii <xtiffany.yin@gmail.com>
2019-04-08 20:08:55 -0700

    fix delete

--- controllers/cupcakesController.js
+++ controllers/cupcakesController.js
@@ -43,5 +43,22 @@ router.put("/api/cupcakes/:id", function(req, res) {
     }
   });
 });
+
+router.delete("/api/cupcakes/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cupcake.delete({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 // Export routes for server.js to use.
 module.exports = router;
